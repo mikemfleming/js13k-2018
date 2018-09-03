0197b46714e032e6d93446c231bed7496482ad5a
miek <flemingatx@gmail.com>
2018-09-03 18:03:44 -0500

    adds basic scoreboard

--- src/index.js
+++ src/index.js
@@ -7,23 +7,39 @@ import Github from './actors/github';
 
 const player = new Player({ x: 250, y: 730 });
 const cat = new Cat({ x: 250, y: 375 });
-const commit = new Commit();
+let commit = new Commit();
 const github = new Github();
+
+const scoreboard = document.createElement('div');
+const scoreText = document.createElement('span');
+let score = 0;
+scoreText.innerText = score;
+scoreboard.appendChild(scoreText);
+document.body.appendChild(scoreboard);
+
 
 const loop = kontra.gameLoop({
 	update () {
+		if (scoreText.innerHTML != score) {
+			scoreText.innerText = score;
+		}
+
 		player.update();
 		const { playerX, playerY } = player.getCenter();
 		const playerDimensions = player.getDimensions();
 
+		// if grabbed, 
+		//	move the commit with the player
 		if (player.isCoding()) {
 			commit.update(playerDimensions);
 		}
 		
+		// if player let goes of grab in github,
+		//	count it towards the score
+		//	generate a new commit
 		if (commit.isPushed() && !player.isCoding()) {
-			console.log('pushin!')
-			//	count it towards the score
-			//  generate a new commit
+			score++;
+			commit = new Commit();
 		}
 
 		cat.update({ playerX, playerY });
